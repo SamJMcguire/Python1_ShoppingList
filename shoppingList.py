439575521764d8aaa6f4a65eb05ec1d5b9514748
SamJMcguire <45981861+SamJMcguire@users.noreply.github.com>
2021-03-18 09:16:31 +1300

    Create shoppingList.py

---
+++ shoppingList.py
@@ -0,0 +1,20 @@
+'''Shopping List exercise
+
+This is a demonstration of how to complete the shopping list task. The shopping list contains user generated lists, boolean while loops, selection
+I always start a program with a docstring'''
+
+#Set variables: user_items as empty list, and add_more as True
+
+#Set user input name - greet user
+
+#Begin loop (loop while add_more True)
+
+#User input  add item to user_items
+
+#add user item
+
+#Selection check add_more is still true - loop or else 
+
+#Print "name" the items on you list are [print list]
+
+
